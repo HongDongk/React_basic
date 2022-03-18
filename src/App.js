62d90bd65f793e6c9a447e67f1de21cb3fa8a65f
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-03-18 14:27:16 +0900

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
@@ -15,16 +12,20 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);   //...currentarray= 기존배열요소에 추가하기
     setToDo("");
   };
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   
+
+  const [coins, setCoins] = useState([]);
+  const getCoins = async () => {
+  const json = await (
+    await fetch(
+      `https://api.coinpaprika.com/v1/tickers`
+    )
+  ).json();
+  setCoins(json);
+  };
+
+  useEffect(() => {getCoins();}, []);
+
   return (
     <div>
       <h1>My ToDos ({toDos.length})</h1>
@@ -32,12 +33,12 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
         <button>Add To Do</button>
       </form>
-
       <ul>
         {toDos.map((item, index) => (<li key={index}>{item}</li>))}
       </ul>
-      <h2>The Coins! {loading ? "" : `(${coins.length})`}</h2>
-      {loading ? (<strong>Loading...</strong>) : (<select>{coins.map((coin) => (<option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}</select> )}
+
+      <h2>The Coins! {`${coins.length}`}</h2>
+      <select>{coins.map((coin) => (<option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}</select> 
     </div>
   )
 }
