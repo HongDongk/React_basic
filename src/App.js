f7cc24abdeb89b8f56bfa2d87bb0ae96dbe52f56
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-03-18 14:01:42 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);   //...currentarray= 기존배열요소에 추가하기
+    setToDo("");
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+        <button>Add To Do</button>
+      </form>
+
+      <ul>
+        {toDos.map((item, index) => (<li key={index}>{item}</li>))}
+      </ul>
+      <h2>The Coins! {loading ? "" : `(${coins.length})`}</h2>
+      {loading ? (<strong>Loading...</strong>) : (<select>{coins.map((coin) => (<option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}</select> )}
     </div>
-  );
+  )
 }
 
 export default App;
